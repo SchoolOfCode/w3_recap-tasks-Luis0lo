a7ab9550e27730780a9de171da87fcf1f2fee0b4
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-12-06 10:54:34 +0000

    share links running -1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //URLs uncomment next line to fetch repos
-// const gitUrl = 'https://api.github.com/users/luis0lo';
+const gitUrl = 'https://api.github.com/users/luis0lo';
 //find out how to access the total amount of contributions
 
 //Selectors
@@ -10,13 +10,50 @@ let data = 0;
 async function getGitHubInfo() {
   const response = await fetch(gitUrl);
   const answer = await response.json();
-//   console.log(answer.public_repos);
+  //   console.log(answer.public_repos);
   data = answer.public_repos;
+  //If you exceed the fetch rate limit
+if (data !== 0) {
+  repos.innerText = data;
+}
 }
 getGitHubInfo();
 
-//If you exceed the fetch rate limit
-if (data !== 0) {
-  repos.innerText = data;
-} 
 
+
+//share links
+// https://crunchify.com/list-of-all-social-sharing-urls-for-handy-reference-social-media-sharing-buttons-without-javascript/
+
+//linkedin PROBLEM!
+//https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+
+//twitter
+// &via=[via] optional Twitter username of content author (don’t include “@”)
+
+const btnLinkedin = document.querySelector('.share-on-linkedin');
+const btnTwitter = document.querySelector('.share-on-twitter');
+const btnWhatsapp = document.querySelector('.share-on-whatsapp');
+
+btnLinkedin.addEventListener('click', shareProfile);
+btnTwitter.addEventListener('click', shareProfile);
+btnWhatsapp.addEventListener('click', shareProfile);
+
+function shareProfile() {
+  let postUrl = encodeURI(document.location.href);
+  let postTitle = encodeURI(`Luis has an interesting profile! 
+  It may fit your needs`);
+  let postHashtag = ['webDeveloper', 'softwareDeveloper'];
+
+  btnLinkedin.setAttribute(
+    'href',
+    `https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`
+  );
+  btnTwitter.setAttribute(
+    'href',
+    `https://twitter.com/share?url=${postUrl}&text=${postTitle}&hashtags=${postHashtag}`
+  );
+  btnWhatsapp.setAttribute(
+    'href',
+    `https://api.whatsapp.com/send?text=${postTitle} ${postUrl}`
+  );
+}
